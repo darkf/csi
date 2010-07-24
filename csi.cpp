7d98229d2b949a88a2a3ab10bd8d8255894800ea
darkf <lw9k123@gmail.com>
2010-07-24 06:12:35 -0500

    Add getcwd command

--- csi.cpp
+++ csi.cpp
@@ -2,6 +2,15 @@
 // license: zlib
 // copyright (c) 2010 darkf
 #include "cube.h"
+#include <unistd.h>
+
+#ifdef WIN32
+#define getcwd _getcwd
+#endif
+
+#ifndef MAX_PATH
+#define MAX_PATH 256
+#endif
 
 ICOMMAND(exit, "", (), exit(0));
 
@@ -16,3 +25,4 @@ void cswait(int secs)
 }
 
 ICOMMAND(wait, "i", (int *secs), cswait(*secs));
+ICOMMAND(getcwd, "", (), { char r[MAX_PATH]; getcwd(r, MAX_PATH); result(r); });
