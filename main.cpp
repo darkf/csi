d6002579e55cf3f3806a14e111d42c46cb0213b2
darkf <lw9k123@gmail.com>
2010-07-25 23:36:44 -0500

    Add optional argument specifying input file

--- main.cpp
+++ main.cpp
@@ -6,10 +6,34 @@
 
 const int BUFSIZE = 1024;
 
-int main()
+int main(int argc, char *argv[])
 {
   char buf[BUFSIZE];
   printf("csi v0.0\n");
+
+  if(argc > 1)
+  {
+      char *filename = argv[1];
+      char *buf;
+      long len;
+      FILE *fd = fopen(filename, "r");
+
+      fseek(fd, 0, SEEK_END);
+      len = ftell(fd);
+      rewind(fd);
+      buf = new char[len];
+      fread(buf, len, 1, fd);
+
+      if(ferror(fd))
+      {
+        printf("couldn't read input file '%s'\n", filename);
+        return 1;
+      }
+
+      execute(buf);
+      delete [] buf;
+      return 0;
+  }
 
   while(true)
   {
